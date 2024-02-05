f805429f5b49fa844b6aed8242dfdb564c518a5c
yeseong31 <yeseong31@naver.com>
2024-02-05 16:00:54 +0900

    feat: 스프링 빈 이름 로그 확인

--- src/main/java/com/example/restfulservice/RestfulServiceApplication.java
+++ src/main/java/com/example/restfulservice/RestfulServiceApplication.java
@@ -1,13 +1,19 @@
 package com.example.restfulservice;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
+@Slf4j
 @SpringBootApplication
 public class RestfulServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(RestfulServiceApplication.class, args);
-	}
+		ApplicationContext applicationContext = SpringApplication.run(RestfulServiceApplication.class, args);
 
+		for (String beanDefinitionName : applicationContext.getBeanDefinitionNames()) {
+			// log.info("bean name: {}", beanDefinitionName);
+		}
+	}
 }

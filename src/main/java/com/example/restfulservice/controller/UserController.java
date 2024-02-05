f56f7e092aec5d43be356fa447f216200d1753e6
yeseong31 <yeseong31@naver.com>
2024-02-05 19:08:18 +0900

    feat: UserController 추가

---
+++ src/main/java/com/example/restfulservice/controller/UserController.java
@@ -0,0 +1,47 @@
+package com.example.restfulservice.controller;
+
+import com.example.restfulservice.exception.UserNotFoundException;
+import com.example.restfulservice.repository.UserRepository;
+import com.example.restfulservice.repository.dto.UserResponseDto;
+import com.example.restfulservice.repository.dto.UserSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("users")
+public class UserController {
+
+    private final UserRepository userRepository;
+
+    @GetMapping
+    public List<UserResponseDto> findAll() {
+        return userRepository.findAll();
+    }
+
+    @PostMapping
+    public ResponseEntity<UserResponseDto> save(@RequestBody UserSaveRequestDto requestDto) {
+        Long savedId = userRepository.save(requestDto);
+
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(savedId)
+                .toUri();
+
+        return ResponseEntity.created(location).build();
+    }
+
+    @GetMapping("/{id}")
+    public UserResponseDto findById(@PathVariable("id") Long id) {
+        return userRepository.findById(id)
+                .orElseThrow(() -> new UserNotFoundException(
+                        String.format("ID[%s] not found", id)));
+    }
+}

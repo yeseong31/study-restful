6ab0fed595a425c4657699acbb88674dab5c4c0f
yeseong31 <yeseong31@naver.com>
2024-02-06 20:54:45 +0900

    feat: Admin 목록 조회 시 필터링 적용

--- src/main/java/com/example/restfulservice/controller/AdminUserController.java
+++ src/main/java/com/example/restfulservice/controller/AdminUserController.java
@@ -16,6 +16,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @Slf4j
 @Validated
 @RestController
@@ -25,22 +27,38 @@ public class AdminUserController {
 
     private final UserService userService;
 
+    @GetMapping
+    public MappingJacksonValue findAll4Admin() {
+        List<AdminUserResponseDto> adminUserResponseDtoResults = userService.findAll().stream()
+                .map(userResponseDto -> {
+                    AdminUserResponseDto adminUserResponseDto = new AdminUserResponseDto();
+                    BeanUtils.copyProperties(userResponseDto, adminUserResponseDto);
+                    return adminUserResponseDto;
+                })
+                .toList();
+
+        MappingJacksonValue mappingJacksonValue = new MappingJacksonValue(adminUserResponseDtoResults);
+        applyJacksonFilter(mappingJacksonValue);
+
+        return mappingJacksonValue;
+    }
+
     @GetMapping("/{id}")
     public MappingJacksonValue findById4Admin(@PathVariable("id") Long id) {
         UserResponseDto userResponseDto = userService.findById(id);
         AdminUserResponseDto adminUserResponseDto = new AdminUserResponseDto();
 
-        // userResponseDto의 정보를 adminUserResponseDto에 복사
         BeanUtils.copyProperties(userResponseDto, adminUserResponseDto);
 
-        // admin에게 보여줄 정보를 필터링
-        SimpleBeanPropertyFilter filter = SimpleBeanPropertyFilter.filterOutAllExcept("id", "name", "joinDate", "ssn");
-        FilterProvider filters = new SimpleFilterProvider().addFilter("UserInfo", filter);
-
-        // 필터링한 결과를 JSON 형식으로 매핑
         MappingJacksonValue mappingJacksonValue = new MappingJacksonValue(adminUserResponseDto);
-        mappingJacksonValue.setFilters(filters);
+        applyJacksonFilter(mappingJacksonValue);
 
         return mappingJacksonValue;
+    }
+
+    private void applyJacksonFilter(MappingJacksonValue mappingJacksonValue) {
+        SimpleBeanPropertyFilter filter = SimpleBeanPropertyFilter.filterOutAllExcept("id", "name", "joinDate", "ssn");
+        FilterProvider filters = new SimpleFilterProvider().addFilter("UserInfo", filter);
+        mappingJacksonValue.setFilters(filters);
     }
 }

06f748fcd9434774a289388b6f0ed8e7e2e449a6
yeseong31 <yeseong31@naver.com>
2024-02-06 21:23:39 +0900

    refactor: 불필요한 `@Setter` 삭제
    DTO의 필드들은 `@Setter`로 주입하는 것이 아니라 "리플렉션"을 이용하여 값을 주입하므로 `@Setter`가 필요 없음

--- src/main/java/com/example/restfulservice/service/dto/UserSaveRequestDto.java
+++ src/main/java/com/example/restfulservice/service/dto/UserSaveRequestDto.java
@@ -5,12 +5,10 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 import static lombok.AccessLevel.PROTECTED;
 
 @Getter
-@Setter
 @NoArgsConstructor(access = PROTECTED)
 public class UserSaveRequestDto {
 

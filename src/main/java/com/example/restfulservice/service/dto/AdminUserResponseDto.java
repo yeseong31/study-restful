19261f78af7654e27098296434b89c87be5e749c
yeseong31 <yeseong31@naver.com>
2024-02-06 18:39:24 +0900

    feat: AdminUserResponseDto 추가

---
+++ src/main/java/com/example/restfulservice/service/dto/AdminUserResponseDto.java
@@ -0,0 +1,36 @@
+package com.example.restfulservice.service.dto;
+
+import com.example.restfulservice.domain.User;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@JsonIgnoreProperties(value = {"password",})
+public class AdminUserResponseDto {
+
+    private Long id;
+
+    @NotBlank
+    @Size(min = 2, max = 20, message = "이름은 2 ~ 20자 이내여야 합니다.")
+    private String name;
+
+    private LocalDateTime joinDate;
+    private String password;
+    private String ssn;
+
+    public AdminUserResponseDto(User entity) {
+        this.id = entity.getId();
+        this.name = entity.getName();
+        this.joinDate = entity.getJoinDate();
+        this.password = entity.getPassword();
+        this.ssn = entity.getSsn();
+    }
+}

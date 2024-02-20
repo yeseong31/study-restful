a6bcf2c46a916b384917dd75ec8ad8acf4ffec1a
yeseong31 <yeseong31@naver.com>
2024-02-20 17:35:32 +0900

    feat: `PostService` 추가

---
+++ src/main/java/com/example/restfulservice/service/PostService.java
@@ -0,0 +1,49 @@
+package com.example.restfulservice.service;
+
+import com.example.restfulservice.domain.Post;
+import com.example.restfulservice.exception.PostNotFoundException;
+import com.example.restfulservice.repository.JpaPostRepository;
+import com.example.restfulservice.service.dto.PostResponseDto;
+import com.example.restfulservice.service.dto.PostSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+import static java.lang.String.format;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class PostService {
+
+    private final JpaPostRepository postRepository;
+
+    @Transactional
+    public Long save(PostSaveRequestDto requestDto) {
+        return postRepository.save(requestDto.toEntity()).getId();
+    }
+
+    public List<PostResponseDto> findAll() {
+        return postRepository.findAll()
+                .stream()
+                .map(PostResponseDto::new)
+                .toList();
+    }
+
+    public PostResponseDto findById(Long id) {
+        Post findPost = postRepository.findById(id)
+                .orElseThrow(() -> new PostNotFoundException(format("ID[%s] not found", id)));
+
+        return new PostResponseDto(findPost);
+    }
+
+    @Transactional
+    public void deleteById(Long id) {
+        postRepository.findById(id)
+                .orElseThrow(() -> new PostNotFoundException(format("ID[%s] not found", id)));
+
+        postRepository.deleteById(id);
+    }
+}

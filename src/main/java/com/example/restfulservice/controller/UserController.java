710901ef71eb8efe27b6c4c4b06662d0b0f4767e
yeseong31 <yeseong31@naver.com>
2024-02-20 16:00:35 +0900

    refactor: 사용자 목록 조회 시 `count` 정보를 응답 JSON에 포함

--- src/main/java/com/example/restfulservice/controller/UserController.java
+++ src/main/java/com/example/restfulservice/controller/UserController.java
@@ -1,6 +1,5 @@
 package com.example.restfulservice.controller;
 
-import com.example.restfulservice.exception.UserNotFoundException;
 import com.example.restfulservice.service.UserService;
 import com.example.restfulservice.service.dto.UserResponseDto;
 import com.example.restfulservice.service.dto.UserSaveRequestDto;
@@ -12,7 +11,6 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.hateoas.CollectionModel;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.http.ResponseEntity;
@@ -21,9 +19,10 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
-import static java.lang.String.format;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
@@ -47,9 +46,14 @@ public class UserController {
             @ApiResponse(responseCode = "500", description = "Internal Server Error")
     })
     @GetMapping
-    public ResponseEntity<CollectionModel<UserResponseDto>> findAll() {
-        List<UserResponseDto> responseDtos = userService.findAll();
-        CollectionModel<UserResponseDto> entityModel = CollectionModel.of(responseDtos);
+    public ResponseEntity<EntityModel<Map<String, Object>>> findAll() {
+        List<UserResponseDto> responseDtoes = userService.findAll();
+
+        Map<String, Object> result = new HashMap<>();
+        result.put("users", responseDtoes);
+        result.put("count", responseDtoes.size());
+
+        EntityModel<Map<String, Object>> entityModel = EntityModel.of(result);
 
         WebMvcLinkBuilder linkTo = linkTo(methodOn(this.getClass()).findAll());
         entityModel.add(linkTo.withRel("all-users"));  // all-users -> http://localhost:8080/users
@@ -117,12 +121,7 @@ public class UserController {
     public ResponseEntity<UserResponseDto> delete(
             @Parameter(description = "사용자 ID", required = true, example = "1") @PathVariable("id") Long id) {
 
-        Long deletedId = userService.deleteById(id);
-
-        if (deletedId == null) {
-            throw new UserNotFoundException(format("ID[%s] not found", id));
-        }
-
+        userService.deleteById(id);
         return ResponseEntity.noContent().build();
     }
 }

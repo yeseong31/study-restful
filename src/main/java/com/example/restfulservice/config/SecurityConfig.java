27c9f0632a7c0a15603f83e495c431fe7596fa09
yeseong31 <yeseong31@naver.com>
2024-02-20 14:46:15 +0900

    fix: H2 콘솔 진입 시 Spring Security 설정 무시

--- src/main/java/com/example/restfulservice/config/SecurityConfig.java
+++ src/main/java/com/example/restfulservice/config/SecurityConfig.java
@@ -2,11 +2,13 @@ package com.example.restfulservice.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 public class SecurityConfig {
@@ -23,6 +25,13 @@ public class SecurityConfig {
         userDetailsManager.createUser(user);
         return userDetailsManager;
     }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return web -> web.ignoring()
+                .requestMatchers(new AntPathRequestMatcher("/h2-console/**"));
+    }
+
 
     @Bean
     BCryptPasswordEncoder passwordEncoder() {
